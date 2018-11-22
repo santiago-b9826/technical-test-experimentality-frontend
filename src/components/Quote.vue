5fd661088fc1c2e430105de095ccdd36b5e35a5e
santiago-b9826 <santiago.b9826@gmail.com>
2018-11-22 13:05:39 -0500

    Final commit, add radio buttons to consume API quotes

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -5,50 +5,33 @@
             <div class="panel-defoutl">
                 <p class="h1">Quotes</p>
                 <p class="h2">Administrate your Quotes</p>
-
-                <form name="Create-Quote-Form">
-                    <div class="form-row">
-                        <div class="form-group col-md-6">
-                            <label class="label">Create Quote: </label>
-                            <button @click="createQuote" class="btn btn-primary">Generate</button> 
-                        </div>
-                      </div>
-                </form>  
-                
-                              
-                <form name="Get-Quote-Form">
-                    <div class="form-row">     
-                        <div class="form-group col-md-6">
-                            <label class="label">Obtain Quote: </label>
-                            <input id='obtainInput' class="form-control" v-model="getQ">
-                            <button @click="getQuote" class="btn btn-primary">Obtain</button>                            
-                        </div>
-                    </div>
-                </form>  
-                
-                <form name="Delete-Quote-Form">
-                    <div class="form-row">   
-                        <div class="form-group col-md-6">
-                            <label class="label">Delete Quote: </label>
-                            <input id='deleteInput' class="form-control" v-model="deleteQ">
-                            <button @click="deleteQuote" class="btn btn-primary">Delete</button> 
-                        </div>
-                    </div>
-                </form> 
-                              
+                <div>
+                  <div v-if="selected && selected!=='Create'">
+                    <input class="h5" type="text" v-model="id" :placeholder="selected"><br>
+                  </div>
+                  <span class="h3" v-if="message">{{ message }}</span>      
+                  <form>
+                    <input type="radio" id="create" value="Create" v-model="selected" @click="clearMessage">
+                    <label class="h4" for="create">Create</label>
+                    <input type="radio" id="obtain" value="Obtain" v-model="selected" @click="clearMessage">
+                    <label class="h4" for="obtain">Obtain</label>
+                    <input type="radio" id="delete" value="Delete" v-model="selected" @click="clearMessage">
+                    <label class="h4" for="delete">Delete</label>
+                  </form>
+                  <button @click="go" class="btn btn-primary">Go</button><br> 
+                </div>                           
             </div>
         </div>
-        <div>
-            <div v-if="quote.quote">          
-                <h5>Id {{quote.id}}</h5>
-                <p>Quote: {{quote.quote}}</p> 
-                <div>
-                    <td><img :src="quote.image" /></td>
-                </div>
-            </div>
-            <div v-else>          
-                <h5>{{quote.message}}</h5>
-            </div>
+    </div>
+    <div class="card">
+        <div class="card-div" v-if="quote.quote">
+            <p>{{quote.id}}- {{quote.quote}}</p> 
+            <div class="image">
+              <img :src="quote.image" />
+            </div>            
+        </div>
+        <div class="card-div" v-else>          
+            <h5>{{quote.message}}</h5>
         </div>
     </div>
   </div>
@@ -64,42 +47,72 @@ export default {
       protocol: "http://",
       host: "localhost:5000",
       path: "/api/v1/generate-changing-life-quote",
-      getQ: "",
-      deleteQ: "",
-      quote: {}
+      id: "",
+      quote: {},
+      selected: "",
+      input: false,
+      message: ""
     };
   },
   methods: {
-    createQuote(e) {
-      e.preventDefault();
+    clearMessage() {
+      this.message = "";
+    },
+    go() {
+      if (this.selected === "Create") {
+        this.createQuote();
+      } else if (this.selected === "Obtain") {
+        if (this.id) {
+          this.getQuote();
+        } else {
+          this.message = "Enter an id";
+        }
+      } else if (this.selected === "Delete") {
+        if (this.id) {
+          this.deleteQuote();
+        } else {
+          this.message = "Enter an id";
+        }
+      } else {
+        this.message = "Select an option";
+      }
+    },
+    createQuote() {
       Axios.post(`${this.protocol}${this.host}${this.path}`).then(response => {
         this.quote = response.data;
       });
     },
-    getQuote(e) {
-      e.preventDefault();
-      if (this.getQ) {
-        Axios.get(`${this.protocol}${this.host}${this.path}/${this.getQ}`).then(
-          response => {
-            this.quote = response.data;
-          }
-        );
-      } else {
-      }
-    },
-    deleteQuote(e) {
-      e.preventDefault();
-      if (this.deleteQ) {
-        Axios.delete(
-          `${this.protocol}${this.host}${this.path}/${this.deleteQ}`
-        ).then(response => {
+    getQuote() {
+      Axios.get(`${this.protocol}${this.host}${this.path}/${this.id}`).then(
+        response => {
           this.quote = response.data;
-        });
-      }
+        }
+      );
+    },
+    deleteQuote() {
+      Axios.delete(`${this.protocol}${this.host}${this.path}/${this.id}`).then(
+        response => {
+          this.quote = response.data;
+        }
+      );
     }
   }
 };
 </script>
 
-<style>
+<style scoped>
+.card-div {
+  left: 30%;
+}
+
+.h4 {
+  width: 5em;
+}
+.image img {
+  height: 45%px;
+  width: 45%;
+}
+.btn.btn.btn-primary{
+  margin: 1em;
+}
 </style>
